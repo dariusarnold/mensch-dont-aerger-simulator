0d9fe08697b5b486b0b428cdfa2032cfe04a4ceb
Darius Arnold <Darius_A@web.de>
2018-06-29 20:13:48 +0200

    Add Token class to Board.py

--- Board.py
+++ Board.py
@@ -12,3 +12,22 @@ class Board:
         self.target_fields = [39, 9, 19, 29]
         self.homes = [list()]*4
 
+class Token:
+    """
+    Token with a player id to which it belongs. Position marks the position on
+    the board.
+    """
+    def __init__(self, position, id):
+        """
+
+        :param position: Current position of token. Either a number from 0-39, 
+        indicating the position on the board, or 'h' for home or -1,-2,-3,-4
+        for the player target
+         for the position in target
+        :type position: int, str
+        :param id: Player id of token, integer from 0...4
+        :type id: int
+        """
+        self.pos = position
+        self.id = id
+        
