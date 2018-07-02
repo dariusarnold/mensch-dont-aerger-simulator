35d0371a41088afb57f92458d940a41f9a733161
Darius Arnold <Darius_A@web.de>
2018-07-02 19:43:51 +0200

    Add basic turn for RandomPlayer, missing: entering target, checking lapse bounds

--- Player.py
+++ Player.py
@@ -1,6 +1,8 @@
 """abstract class Player, derive from it and implement players with different 
 strategies"""
 
+from random import randint, choice
+from Board import InvalidMoveException
 
 class Player:
 
@@ -45,4 +47,22 @@ class RandomPlayer(Player):
         super().__init__(id)
 
     def turn(self, board, dice_roll):
-        pass
+        home_tokens = board.get_home_tokens(self.id)
+        available_tokens = board.get_player_tokens_on_board(self.id)
+        if dice_roll == 6 and home_tokens:
+            # need to get out of the home if start field is free
+            try:
+                board.move_out_of_home(self.id)
+            except InvalidMoveException as ive:
+                # start field blocked by own token, skip this 6
+                pass
+
+        elif available_tokens:
+            picked = self.select_random_token(available_tokens)
+            board.move_token(picked, dice_roll)
+
+    def select_random_token(self, token_list):
+        """Return one random token from token_list"""
+        #print(len(token_list))
+        return choice(token_list)
+        #return token_list[randint(0, len(token_list))]
